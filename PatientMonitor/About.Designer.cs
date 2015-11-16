9d119eb5a38b74eadbdbcc7d350e1e1af89ef953
wollac11 <wollac11@gmail.com>
2015-11-16 21:30:48 +0000

    Add about screen

---
+++ PatientMonitor/About.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PatientMonitor
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAboutTitle = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblAboutDetail = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAboutTitle
+            // 
+            this.lblAboutTitle.AutoSize = true;
+            this.lblAboutTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAboutTitle.Location = new System.Drawing.Point(13, 13);
+            this.lblAboutTitle.Name = "lblAboutTitle";
+            this.lblAboutTitle.Size = new System.Drawing.Size(184, 20);
+            this.lblAboutTitle.TabIndex = 0;
+            this.lblAboutTitle.Text = "About Patient Monitor";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(14, 46);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(60, 13);
+            this.lblVersion.TabIndex = 1;
+            this.lblVersion.Text = "Version 0.1";
+            // 
+            // lblAboutDetail
+            // 
+            this.lblAboutDetail.Location = new System.Drawing.Point(14, 84);
+            this.lblAboutDetail.Name = "lblAboutDetail";
+            this.lblAboutDetail.Size = new System.Drawing.Size(258, 47);
+            this.lblAboutDetail.TabIndex = 2;
+            this.lblAboutDetail.Text = "An application to monitor patient vitals and work shifts of medical staff";
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblAboutDetail);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.lblAboutTitle);
+            this.Name = "About";
+            this.Text = "About";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAboutTitle;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblAboutDetail;
+    }
+}
