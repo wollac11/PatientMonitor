e6d257639d57a31782fe4139464b000b043ec968
wollac11 <wollac11@gmail.com>
2015-12-09 05:20:38 +0000

    Cleaned up TimeRecorder csvWiter and fixed redundant file creation

--- PatientMonitor/TimerRecorder.cs
+++ PatientMonitor/TimerRecorder.cs
@@ -22,52 +22,25 @@ namespace PatientMonitor
         AlarmBelow alarmBelow = new AlarmBelow();
 
         /// <summary>
-        /// write timers to csv
+        /// Write timers to CSV
         /// </summary>
         public void csvWriter(string timer)
         {
-            //path to where the file will save
-            //got help from:
-            //http://stackoverflow.com/questions/20997716/how-to-edit-text-file-in-resources-folder-of-window-form-c-sharp
-            string fileAddress = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TimerRecords.csv");
+            // Path to save file
+            string fileAddress = "Timer Records.csv";
 
-            //delimiter to space the time apart
+            // Time record delimiter
             string delimiter = ",";
 
-            string file = "Timer Records.csv";
-            //create an array to store time
+            // Create an array to store time
             string[] timeData = new string[] { "Time taken to disable alarm:", timer };
 
-            //create a string builder to use for the array
+            // Build CSV string to use for the array
             StringBuilder stringbuild = new StringBuilder();
+            stringbuild.AppendLine(string.Join(delimiter, timer));
 
-            //write to file
-            using (StreamWriter sw = new StreamWriter("TimerRecords.csv"))
-            {
-                stringbuild.AppendLine(string.Join(delimiter, timer));
-                File.AppendAllText(file, stringbuild.ToString());
-
-            }
-
-
-            /*
-            //create an array to collect both alarms that go above and below limits
-            string[][] alarmTimes = new string[][]
-            {
-                new string [] {"Alarm times gone above limits", above},
-                new string [] {"Alarm times gone below limits", _below}
-            };
-
-            int arrayLength = alarmTimes.GetLength(0);
-            StringBuilder stringbuild = new StringBuilder();
-            for (int i = 0; i < arrayLength; i++)
-            {
-                stringbuild.AppendLine(string.Join(delimiter, alarmTimes[i]));
-            }
-
+            // Append string to file
             File.AppendAllText(fileAddress, stringbuild.ToString());
-            */
-
         }
     }
 }
